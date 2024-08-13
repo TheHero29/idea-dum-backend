a15c63fc1c68e924b8c5c4903b123ed1feb3b3ed
TheHero29 <loukik.23bcs10059@ms.sst.scaler.com>
2024-08-14 03:57:24 +0530

    push

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,16 +3,24 @@ const postModel = require("../models/postModel.js");
 exports.getPostById = async (req, res) => {
   try {
     const post = await postModel.findById({ _id: req.params.id });
-    if (!post) return res.status(404).send("post with given ID not found");
+    if (!post)
+      return res
+        .status(404)
+        .send({ success: false, message: "post with given ID not found" });
     res.send(post);
   } catch (err) {
-    return res.status(404).send("post with given ID not found");
+    return res
+      .status(404)
+      .send({ success: false, message: "post with given ID not found" });
   }
 };
 exports.getAllPosts = async (req, res) => {
   try {
     const posts = await postModel.find({});
-    if (posts == []) return res.status(404).send("No posts available");
+    if (posts == [])
+      return res
+        .status(404)
+        .send({ success: false, message: "No posts available" });
     posts.sort((a, b) => {
       const voteDifferenceA = a.upvotes - a.downvotes;
       const voteDifferenceB = b.upvotes - b.downvotes;
@@ -20,14 +28,18 @@ exports.getAllPosts = async (req, res) => {
     });
     res.send(posts);
   } catch (err) {
-    return res.status(404).send("error in fetching posts");
+    return res
+      .status(404)
+      .send({ success: false, message: "error in fetching posts" });
   }
 };
 exports.createPost = async (req, res) => {
   try {
     const body = req.body;
     if (!body.title || !body.content || !body.author)
-      return res.status(404).send("Please enter all the details");
+      return res
+        .status(404)
+        .send({ success: false, message: "Please enter all the details" });
     const post = postModel.create({
       title: body.title,
       content: body.content,
@@ -36,9 +48,14 @@ exports.createPost = async (req, res) => {
       image: body.image,
     });
     console.log("post created successfully");
-    return res.status(201).json({ message: "post created successfully" });
+    return res
+      .status(201)
+      .json({ success: true, message: "post created successfully" });
   } catch (err) {
-    return res.status(404).send("Some error occured while creating post");
+    return res.status(404).send({
+      success: false,
+      message: "Some error occured while creating post",
+    });
   }
 };
 
